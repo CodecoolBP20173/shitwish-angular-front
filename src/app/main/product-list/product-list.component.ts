dc232308f87e5b78ee42333471a4d35641d9ef52
Horváth Máté <ifjmate@gmail.com>
2018-07-18 12:29:12 +0200

    added grid to product list and scrolling to long descriptions

--- src/app/main/product-list/product-list.component.ts
+++ src/app/main/product-list/product-list.component.ts
@@ -11,38 +11,39 @@ import {Subscription} from 'rxjs';
 export class ProductListComponent implements OnInit, OnDestroy {
 
     public productList: Product[] = [];
-    private productSubscription: Subscription;
+
+    //private productSubscription: Subscription;
 
     constructor(private productService: ProductsService) {
-        // this.productList.push({
-        //     id: 1,
-        //     category: 'Tablet',
-        //     description: 'This is a very bad tablet. If you are looking for slow and half broken product feel free to buy it.',
-        //     imageUrl: 'https://static11.edstatic.net/product_images/280x210/resize/ifhd_dsahm9vj.jpg?v=1',
-        //     name: 'Shitty Tablet',
-        //     price: 499,
-        //     userId: 'myID'
-        // });
-        // this.productList.push({
-        //     id: 1,
-        //     category: 'Tablet',
-        //     description: 'This is a very bad tablet. If you are looking for slow and half broken product feel free to buy it.',
-        //     imageUrl: 'https://static11.edstatic.net/product_images/280x210/resize/ifhd_dsahm9vj.jpg?v=1',
-        //     name: 'Shitty Tablet',
-        //     price: 499,
-        //     userId: 'myID'
-        // });
+        this.productList.push({
+            id: 1,
+            category: 'Tablet',
+            description: 'kkkkkkkkljlkhkjgjhhmh,kj,gkjgkhmvjncuhjghkmvhjvnkldnflkdsnfkldsnflkdsnfkldsnfkldsnflkdsnflksdfThis is a very bad tablet. If you are looking for slow and half broken product feel free to buy it.',
+            imageUrl: 'https://static11.edstatic.net/product_images/280x210/resize/ifhd_dsahm9vj.jpg?v=1',
+            name: 'Shitty Tablet',
+            price: 499,
+            userId: 'myID'
+        });
+        this.productList.push({
+            id: 1,
+            category: 'Tablet',
+            description: 'This is a very bad tablet. If you are looking for slow and half broken product feel free to buy it.',
+            imageUrl: 'https://static11.edstatic.net/product_images/280x210/resize/ifhd_dsahm9vj.jpg?v=1',
+            name: 'Shitty Tablet',
+            price: 499,
+            userId: 'myID'
+        });
     }
 
     ngOnInit() {
-        this.productSubscription = this.productService.products.subscribe(
-            products => this.productList = products
-        );
-        this.productService.fetchProducts();
+        // this.productSubscription = this.productService.products.subscribe(
+        //     products => this.productList = products
+        // );
+        // this.productService.fetchProducts();
     }
 
     ngOnDestroy() {
-        this.productSubscription.unsubscribe();
+        // this.productSubscription.unsubscribe();
     }
 
 }

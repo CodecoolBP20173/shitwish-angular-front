03f67ee404cdfabe1de2c98fe7c9b375ad29488b
Horváth Máté <ifjmate@gmail.com>
2018-07-18 11:24:36 +0200

    connected product service to product list

--- src/app/main/product-list/product-list.component.ts
+++ src/app/main/product-list/product-list.component.ts
@@ -1,37 +1,48 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Product} from '../../models';
+import {ProductsService} from '../../services/products.service';
+import {Subscription} from 'rxjs';
 
 @Component({
     selector: 'app-product-list',
     templateUrl: './product-list.component.html',
     styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent implements OnInit {
+export class ProductListComponent implements OnInit, OnDestroy {
 
     public productList: Product[] = [];
+    private productSubscription: Subscription;
 
-    constructor() {
-        this.productList.push({
-            id: 1,
-            category: 'Tablet',
-            description: 'This is a very bad tablet. If you are looking for slow and half broken product feel free to buy it.',
-            imageUrl: 'https://static11.edstatic.net/product_images/280x210/resize/ifhd_dsahm9vj.jpg?v=1',
-            name: 'Shitty Tablet',
-            price: 499,
-            userId: 'myID'
-        });
-        this.productList.push({
-            id: 1,
-            category: 'Tablet',
-            description: 'This is a very bad tablet. If you are looking for slow and half broken product feel free to buy it.',
-            imageUrl: 'https://static11.edstatic.net/product_images/280x210/resize/ifhd_dsahm9vj.jpg?v=1',
-            name: 'Shitty Tablet',
-            price: 499,
-            userId: 'myID'
-        });
+    constructor(private productService: ProductsService) {
+        // this.productList.push({
+        //     id: 1,
+        //     category: 'Tablet',
+        //     description: 'This is a very bad tablet. If you are looking for slow and half broken product feel free to buy it.',
+        //     imageUrl: 'https://static11.edstatic.net/product_images/280x210/resize/ifhd_dsahm9vj.jpg?v=1',
+        //     name: 'Shitty Tablet',
+        //     price: 499,
+        //     userId: 'myID'
+        // });
+        // this.productList.push({
+        //     id: 1,
+        //     category: 'Tablet',
+        //     description: 'This is a very bad tablet. If you are looking for slow and half broken product feel free to buy it.',
+        //     imageUrl: 'https://static11.edstatic.net/product_images/280x210/resize/ifhd_dsahm9vj.jpg?v=1',
+        //     name: 'Shitty Tablet',
+        //     price: 499,
+        //     userId: 'myID'
+        // });
     }
 
     ngOnInit() {
+        this.productSubscription = this.productService.products.subscribe(
+            products => this.productList = products
+        );
+        this.productService.fetchProducts();
+    }
+
+    ngOnDestroy() {
+        this.productSubscription.unsubscribe();
     }
 
 }

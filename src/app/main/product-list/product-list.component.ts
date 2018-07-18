b501dcdfb025adbe4a2a8482118642b1fd3df7cf
Horváth Máté <ifjmate@gmail.com>
2018-07-18 11:14:30 +0200

    connected product with product list

--- src/app/main/product-list/product-list.component.ts
+++ src/app/main/product-list/product-list.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Product} from '../../models';
 
 @Component({
-  selector: 'app-product-list',
-  templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+    selector: 'app-product-list',
+    templateUrl: './product-list.component.html',
+    styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
 
-  constructor() { }
+    public productList: Product[] = [];
 
-  ngOnInit() {
-  }
+    constructor() {
+        this.productList.push({
+            id: 1,
+            category: 'Tablet',
+            description: 'This is a very bad tablet. If you are looking for slow and half broken product feel free to buy it.',
+            imageUrl: 'https://static11.edstatic.net/product_images/280x210/resize/ifhd_dsahm9vj.jpg?v=1',
+            name: 'Shitty Tablet',
+            price: 499,
+            userId: 'myID'
+        });
+        this.productList.push({
+            id: 1,
+            category: 'Tablet',
+            description: 'This is a very bad tablet. If you are looking for slow and half broken product feel free to buy it.',
+            imageUrl: 'https://static11.edstatic.net/product_images/280x210/resize/ifhd_dsahm9vj.jpg?v=1',
+            name: 'Shitty Tablet',
+            price: 499,
+            userId: 'myID'
+        });
+    }
+
+    ngOnInit() {
+    }
 
 }
--- src/app/main/product-list/product/product.component.ts
+++ src/app/main/product-list/product/product.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {Product} from '../../../models';
 
 @Component({
@@ -8,18 +8,10 @@ import {Product} from '../../../models';
 })
 export class ProductComponent implements OnInit {
 
+    @Input('product')
     public product: Product;
 
     constructor() {
-        this.product = {
-            id: 1,
-            category: 'Tablet',
-            description: 'This is a very bad tablet. If you are looking for slow and half broken product feel free to buy it.',
-            imageUrl: 'https://static11.edstatic.net/product_images/280x210/resize/ifhd_dsahm9vj.jpg?v=1',
-            name: 'Shitty Tablet',
-            price: 499,
-            userId: 'myID'
-        };
     }
 
     ngOnInit() {

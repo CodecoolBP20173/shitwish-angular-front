75f3efda75478b79408647b44400c848f3f81ab1
DanielCs88 <daniel.cs20@gmail.com>
2018-07-18 11:08:09 +0200

    Product service.

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import {BehaviorSubject, Subject} from 'rxjs';
+import {debounceTime, distinctUntilChanged} from 'rxjs/operators';
+import {Product} from '../models';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
 
-  constructor() { }
+    products = new BehaviorSubject<Product[]>([]);
+    searchTerms = new Subject<string>();
+    private readonly PRODUCTS_ENDPOINT = '';
+
+    constructor(private http: HttpClient) {
+        this.searchTerms.pipe(
+            debounceTime(300),
+            distinctUntilChanged(),
+        ).subscribe((term: string) => this.fetchProducts(term));
+    }
+
+    fetchProducts(term?: string) {
+        // TODO: make sure the term is uri encoded
+        this.http.get<Product[]>(`${this.PRODUCTS_ENDPOINT}/?${term ? `query=${term}&` : ''}limit=20&offset=0`)
+            .subscribe(products => this.products.next(products));
+    }
 }

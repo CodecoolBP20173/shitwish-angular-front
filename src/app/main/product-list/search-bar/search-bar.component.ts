c2352eb06342b7a5a7701b1be90532f6b09fa888
Adam Feher <fhradm@gmail.com>
2018-07-18 10:24:54 +0200

    search-bar template

--- src/app/main/product-list/search-bar/search-bar.component.ts
+++ src/app/main/product-list/search-bar/search-bar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {faSearch} from '@fortawesome/free-solid-svg-icons';
+
 
 @Component({
   selector: 'app-search-bar',
@@ -6,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-bar.component.css']
 })
 export class SearchBarComponent implements OnInit {
+    faSearch = faSearch;
 
   constructor() { }
 

663c9077b4af3e607eb8459a71806eb367c5b1d0
DanielCs88 <daniel.cs20@gmail.com>
2018-07-18 10:07:17 +0200

    User service.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,28 +11,29 @@ import { OrderConfirmationComponent } from './main/order-confirmation/order-conf
 import { SearchBarComponent } from './main/product-list/search-bar/search-bar.component';
 import { ProductComponent } from './main/product-list/product/product.component';
 import {AppRoutingModule} from './app-routing.module';
-import {HTTP_INTERCEPTORS} from '@angular/common/http';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import {AuthInterceptor} from './auth/auth.interceptor';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavbarComponent,
-    ShoppingCartComponent,
-    MainComponent,
-    ProductListComponent,
-    CheckoutComponent,
-    OrderConfirmationComponent,
-    SearchBarComponent,
-    ProductComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [
-      {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true}
-  ],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        NavbarComponent,
+        ShoppingCartComponent,
+        MainComponent,
+        ProductListComponent,
+        CheckoutComponent,
+        OrderConfirmationComponent,
+        SearchBarComponent,
+        ProductComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HttpClientModule
+    ],
+    providers: [
+        {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true}
+    ],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,15 +1,23 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {User} from '../models';
+import {BehaviorSubject} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+    private readonly USERS_ENDPOINT = '';
+    user = new BehaviorSubject<User>(null);
+
+    constructor(private http: HttpClient) { }
 
     getUserProfile() {
+        this.http.get<User>(this.USERS_ENDPOINT).subscribe(user => this.user.next(user));
     }
 
     removeUserProfile() {
+        this.user.next(null);
     }
 }

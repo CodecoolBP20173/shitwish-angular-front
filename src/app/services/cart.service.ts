d7a8aa5c2514ffa62a0e974e8470dda0d6c2b185
Horváth Máté <ifjmate@gmail.com>
2018-07-19 14:42:53 +0200

    added httpClient injection

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {BehaviorSubject} from 'rxjs';
 import {Cart} from '../models';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,8 @@ export class CartService {
 
     cart = new BehaviorSubject<Cart>(null);
 
-    constructor() { }
+    constructor(private http: HttpClient) {
+    }
 
     addToCart(id: number) {
 
